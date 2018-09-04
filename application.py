d3e3eefa610db459916dc09bdebe78dae8b61930
jeffninghan <jeffninghan@gmail.com>
2018-09-03 23:06:06 -0700

    status page

--- application.py
+++ application.py
@@ -1,10 +1,20 @@
+import subprocess
+
 from flask import Flask, render_template
+
 application = Flask(__name__)
  
 @application.route('/')
 @application.route('/index')
 def index():
     return render_template('index.html', user_image='/static/funny_clare.png')
+
+
+@application.route('/status')
+def status():
+  status = subprocess.check_output(['bash','-c', "awk '/./{line=$0} END{print line}' /home/ec2-user/instagram/claresfoodjourney.log"])
+  return render_template('status.html', status=status)
+
  
 if __name__ == '__main__':
     application.run()
